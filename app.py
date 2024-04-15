cb7e388abb399c35d11ccdb0276488d16afb6705
Nammi <knsridha@usc.edu>
2024-04-15 16:11:44 -0700

    fix analysis output

--- app.py
+++ app.py
@@ -10,11 +10,8 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 cur_dir = os.path.dirname(os.path.realpath(__file__))
-UPLOAD_FOLDER = os.path.join(cur_dir, 'temp')
-ALLOWED_EXTENSIONS = {'csv'}
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = b'Placeholder'
 
 @app.route("/")
@@ -37,13 +34,6 @@ def database_html():
             else:
                 flash(f"Executing {cmd.command} {cmd.arg1}", 'primary')
                 analysis_out = run_analyses(cmd)
-
-            #return redirect(url_for('database_html'))
-        
-        elif data.get('analyses'):
-            analysis_out = run_analyses(data.get('analyses'))
-        else:
-            pass
         print(analysis_out)
     return render_template("database.html", data=html, cat_output=cat_out, analyses_output=analysis_out)
 
--- helper.py
+++ helper.py
@@ -1,9 +1,8 @@
 from typing import Any
 from collections import namedtuple
-from app import UPLOAD_FOLDER
 import requests
 from flask import Flask, render_template
-
+import pandas as pd
 
 def validate_input(form_response: dict, file_obj) -> tuple:
     """Validates input from form
@@ -72,6 +71,7 @@ def run_analyses(command: tuple) -> str:
         query = f"SELECT DISTINCT callsign, origin_airport_name, destination_airport_name from flight_data where latitude < 0 limit 10"
         desc = "Finds all Delta flights within the Southern hemisphere"
         result = requests.post("http://127.0.0.1:8090/database/scan", json={"tableName": "flight_data", "selectQuery": query}).json()
+    #result = [{'callsign': 'DAL88', 'origin_airport_name': 'Atlanta Hartsfield-Jackson International Airport', 'destination_airport_name': 'Paris Charles de Gaulle Airport'}, {'callsign': 'DAL84', 'origin_airport_name': 'Atlanta Hartsfield-Jackson International Airport', 'destination_airport_name': 'Paris Charles de Gaulle Airport'}, {'callsign': 'DAL98', 'origin_airport_name': 'Detroit Metropolitan Wayne County Airport', 'destination_airport_name': 'Paris Charles de Gaulle Airport'}, {'callsign': 'DAL228', 'origin_airport_name': 'Cincinnati Northern Kentucky International Airport', 'destination_airport_name': 'Paris Charles de Gaulle Airport'}, {'callsign': 'DAL266', 'origin_airport_name': 'New York John F. Kennedy International Airport', 'destination_airport_name': 'Paris Charles de Gaulle Airport'}, {'callsign': 'DAL82', 'origin_airport_name': 'Atlanta Hartsfield-Jackson International Airport', 'destination_airport_name': 'Paris Charles de Gaulle Airport'}, {'callsign': 'DAL264', 'origin_airport_name': 'New York John F. Kennedy International Airport', 'destination_airport_name': 'Paris Charles de Gaulle Airport'}, {'callsign': 'DAL152', 'origin_airport_name': 'Minneapolis Saint Paul International Airport', 'destination_airport_name': 'Paris Charles de Gaulle Airport'}, {'callsign': 'DAL224', 'origin_airport_name': 'Boston Logan International Airport', 'destination_airport_name': 'Paris Charles de Gaulle Airport'}, {'callsign': 'DAL220', 'origin_airport_name': 'Salt Lake City International Airport', 'destination_airport_name': 'Paris Charles de Gaulle Airport'}]
     return format_analyses_output(result, query, desc)
         
 
@@ -86,37 +86,9 @@ def format_analyses_output(analyses_result, query, desc) -> str:
     header = f"<h1>Analyses</h1><h2>What Analyses is being ran?</h2><p>{query}</p>"
     desc = f"<h2>What is this Analyses doing?</h2><p>{desc}</p>"
     note = f"<div class='alert alert-warning alert-dismissable fade show' role='alert'><strong>NOTE:</strong> <button type='button' class='close' data-dismiss='alert' aria-label='close><span aria-hidden='true'>&times;</span></button></div>"
-    final_output = f"<h2>Final Output of Analyses</h2>{repr_list_items(analyses_result)}"
+    final_output = f"<h2>Final Output of Analyses</h2>{pd.DataFrame(analyses_result).to_html(index = False)}"
     html_repr = f"{header}{desc}{final_output}"
     return html_repr
-
-def repr_list_items(li: list) -> str:
-    list_repr = f""
-    if isinstance(li, dict):
-        for key, value in li.items():
-            item_repr = f"<p style=padding-left:15px>{key} : {value},</p>"
-            list_repr += item_repr
-    # Otherwise, iterate through elements in list
-    else:
-        for item in li:
-            list_repr += "<div style=padding-left:30px>["
-            # List item could be a dictionary
-            if not isinstance(item, list):
-                for key, value in item.items():
-                    if isinstance(value, dict):
-                        val_repr = repr_list_items(value)
-                    else:
-                        val_repr = value
-                    item_repr = f"<p style='padding-left:15px font-size:8px'>{key} : {val_repr},</p>"
-                    list_repr += item_repr
-            # List item is another list
-            else:
-                list_repr += "<div style=padding-left:30px>["
-                list_repr += repr_list_items(item)
-                list_repr += "],</div>"
-            list_repr += "],</div>"
-    return list_repr
-
 
 if __name__ == "__main__":
     # This area is used for testing functions by running python helper.py
